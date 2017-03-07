ab0b7ebf17bf98bb32b3a00d0fcfb7315ee26594
Rainer Bieniek <Rainer.Bieniek@vodafone.com>
2017-03-07 10:46:06 +0100

    Add build for java archive containing the extension

---
+++ spring-cdi-bridge/src/test/java/de/bieniekconsulting/springcdi/bridge/support/TestJarBuilder.java
@@ -0,0 +1,21 @@
+package de.bieniekconsulting.springcdi.bridge.support;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
+import de.bieniekconsulting.springcdi.bridge.CdiScope;
+import de.bieniekconsulting.springcdi.bridge.DependencyRegisteringBeanFactoryPostProcessor;
+import de.bieniekconsulting.springcdi.bridge.SpringBean;
+import de.bieniekconsulting.springcdi.bridge.SpringCdiExtension;
+import de.bieniekconsulting.springcdi.bridge.spring.Cdi;
+import de.bieniekconsulting.springcdi.bridge.spring.SpringScoped;
+
+public class TestJarBuilder {
+	public static JavaArchive extensionJar() {
+		return ShrinkWrap.create(JavaArchive.class).addClass(CdiScope.class)
+				.addClass(DependencyRegisteringBeanFactoryPostProcessor.class).addClass(SpringBean.class)
+				.addClass(SpringCdiExtension.class).addClass(Cdi.class).addClass(SpringScoped.class)
+				.addClass(ApplicationContextProvider.class).addAsManifestResource(
+						"services/de.bieniekconsulting.springcdi.bridge.support.ApplicationContextProvider");
+	}
+}
---
+++ wildfly-test/src/main/java/de/bieniekconsulting/springcdi/wildfly/spring/SpringGreeter.java
@@ -0,0 +1,5 @@
+package de.bieniekconsulting.springcdi.wildfly.spring;
+
+public class SpringGreeter {
+
+}
